434c7fe5ba8bac33f3c0c8a0514b3d950f8bbbc7
Mahendra Patel <Mpatel6695@conestogac.on.ca>
2024-02-07 11:45:56 -0500

    Add project files.

---
+++ PoC_01_HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+            Console.WriteLine(11 + 11);
+            Console.WriteLine(55 - 2);
+            Console.WriteLine(5 * 2);
+            Console.WriteLine(1.9 / 2);
+        }
+    }
+}
